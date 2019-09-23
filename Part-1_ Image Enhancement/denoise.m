cab262e4c95538d73f5d4012ddd15f8cbf0c24ae
Sasafrass <albertharkema@gmail.com>
2019-09-23 16:49:11 +0200

    box filter

--- Part-1_ Image Enhancement/denoise.m
+++ Part-1_ Image Enhancement/denoise.m
@@ -1,7 +1,11 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
 
+I = imread(image);
+
 switch kernel_type
     case 'box'
+        imOut = imboxfilt(I, 3);
+        imshow(imOut);
         fprintf('Not implemented\n')
     case 'median'
         fprintf('Not implemented\n')
